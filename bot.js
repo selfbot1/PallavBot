f826e77361380df2e42ededf220e4f078af2880f
PallavBS <32361541+PallavBS@users.noreply.github.com>
2017-10-26 03:04:09 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const responseObject = {
-  "hi": "Hey!",
-  "?": "Say what?",
-  "lol": "roflmaotntpmp"
-};
+
 // Set the prefix
 let prefix = "p";
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
-    if(responseObject[message.content]) {
-    message.channel.send(responseObject[message.content]);}
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   if (message.content.startsWith(prefix + "ping")) {
@@ -19,11 +13,6 @@ client.on("message", (message) => {
   if (message.content.startsWith(prefix + "foo")) {
     message.channel.send("bar!");
   }
- 
-x
-  }
 });
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
