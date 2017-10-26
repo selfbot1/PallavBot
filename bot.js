93105c18d4b73ccd623b228500fdcd33b29afb6f
PallavBS <32361541+PallavBS@users.noreply.github.com>
2017-10-25 23:22:00 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,12 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
+        message.reply('pong');
+        message.reply('pong');
+        message.reply('pong');
+        message.reply('pong');
+        message.reply('pong');
+       
   	}
 });
 
