ec0f7d7c0e90c710fc71120769fc4d47cf4a909f
PallavBS <32361541+PallavBS@users.noreply.github.com>
2017-10-26 03:09:46 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.user.setGame('with Pallav.')
 // Set the prefix
 let prefix = "p";
 client.on("message", (message) => {
