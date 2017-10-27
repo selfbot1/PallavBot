b5f490f2ca0c1d341f3981411117f9a07e20fc2d
PallavBS <32361541+PallavBS@users.noreply.github.com>
2017-10-27 12:55:52 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,10 @@ const client = new Discord.Client();
 // Set the prefix
 let prefix = "p";
 
-client.on('ready', () => {
+client.on("ready", () => {
+  console.log("I am ready!");
   client.setGame("Brawl Stars", "https://go.twitch.tv/pallavbs"); // a game
 
-  console.log('I am ready!');
 });
 
 // Create an event listener for new guild members
@@ -18,18 +18,15 @@ client.on('guildMemberAdd', member => {
   // Send the message, mentioning the member
   channel.send(`Welcome to the server, ${member}`);
 });
-
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-
+  
   if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "spam")) {
-    while counter < 1000
-    message.channel.send("bar!");
   }
+});
+
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
