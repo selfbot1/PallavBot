d888010ed28879bb242922da229c27427be41e6f
PallavBS <32361541+PallavBS@users.noreply.github.com>
2017-10-27 13:10:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ let prefix = "p";
 
 client.on("ready", () => {
   console.log("I am ready!");
+  client.user.setPresence({ status: 'Do Not Disturb', game: { name: 'with Pallav.' } });
 //  client.setGame("Brawl Stars", "https://go.twitch.tv/pallavbs"); // a game
 
 });
